8e261f880bd2b93b4709c4c602990dce82c49b29
devandrewjpn <yuutoandrew.jpn@gmail.com>
2022-12-27 20:53:58 -0300

    dropdownb ugou

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ import dancingButton from "./modules/dancingButton.js";
 import tabMenu from "./modules/sidemenu.js";
 import modal from "./modules/modal.js";
 import toolTip from "./modules/tooltip.js";
+import dropdown from "./modules/dropdown.js"
 
 tabMenu();
 modal();
 dancingButton();
 toolTip();
+dropdown();
 
 document.addEventListener('visibilitychange', (event) => {
     if (document.visibilityState === 'visible') {
