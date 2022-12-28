58f97dfed80c402e4619c783646cd75858307439
devandrewjpn <yuutoandrew.jpn@gmail.com>
2022-12-27 22:10:46 -0300

    cronometro

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ import tabMenu from "./modules/sidemenu.js";
 import modal from "./modules/modal.js";
 import toolTip from "./modules/tooltip.js";
 import dropdown from "./modules/dropdown.js"
+import cronometro from "./modules/cronometro.js"
 
 tabMenu();
 modal();
 dancingButton();
 toolTip();
 dropdown();
+cronometro();
 
 document.addEventListener('visibilitychange', (event) => {
     if (document.visibilityState === 'visible') {
---
+++ modules/cronometro.js
@@ -0,0 +1,30 @@
+export default function cronometro() {
+    const pause = document.querySelector('[data-cronometro="pause"]');
+    const start = document.querySelector('[data-cronometro="start"]');
+    const reset = document.querySelector('[data-cronometro="reset"]');
+    const tempo = document.querySelector('[data-cronometro="time"]');
+
+    let i = 0;
+    let timer;
+
+    function startTime() {
+        timer = setInterval(() => {
+            tempo.innerText = i++;
+        }, 100);
+        start.setAttribute('disabled', '');
+    }
+
+    function pauseTime() {
+        clearInterval(timer);
+        start.removeAttribute('disabled');
+    }
+
+    function resetTime() {
+        tempo.innerText = 0;
+        i = 0;
+    }
+
+    start.addEventListener('click', startTime);
+    pause.addEventListener('click', pauseTime);
+    reset.addEventListener('click', resetTime);
+}
